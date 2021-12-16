0b7c63d81568cc36e8858e5ad6cfc8144cc68cbc
Anton Pieper <94118572+AntonPieper@users.noreply.github.com>
2021-12-16 01:35:14 +0100

    feat: add window (#22)
    * feat: add window

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
-#include <stdio.h>
+#include <ncurses.h>
 
 int main() {
-  printf("Hello World!\n");
-  getchar();
+  const WINDOW *window = initscr();
+
+  do {
+    clear();
+    int maxX = getmaxx(window);
+    int maxY = getmaxy(window);
+    for (int y = 0; y < maxY; ++y)
+      for (int x = 0; x < maxX; ++x) {
+        if (x % (maxX / 10) == 0 || y % (maxY / 10) == 0)
+          mvaddch(y, x, '.');
+      }
+    refresh();
+  } while (getch() == KEY_RESIZE);
+  endwin();
   return 0;
 }
