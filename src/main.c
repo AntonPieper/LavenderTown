af0dd5a104e0b680ab7a8fd711432e65b01662ae
AntonPieper <anton.pieper@informatik.hs-fulda.de>
2021-12-18 12:24:20 +0100

    feat: simple cross platform curses program

--- src/main.c
+++ src/main.c
@@ -1,19 +1,48 @@
 #include <curses.h>
+#include <string.h>
+
+const int GREET_COLOR = 1;
+const int NAME_INPUT_COLOR = 2;
 
 int main() {
   WINDOW *window = initscr();
+  if (has_colors() == FALSE) {
+    endwin();
+    printf("Your Terminal does not support color!\n");
+    exit(1);
+  }
+  start_color();
+  init_pair(GREET_COLOR, COLOR_YELLOW, COLOR_BLACK);
+  init_pair(NAME_INPUT_COLOR, COLOR_MAGENTA, COLOR_BLACK);
+  int rows, columns;
+  // A Macro for (rows = getmaxx(window), columns=getmaxy(window))
+  getmaxyx(window, rows, columns);
+  printw("Hello World %s!", "from me"); // Works just like printf(...)
+  char message[] = "Hello World from center of screen";
+  mvprintw(rows / 2, (columns - strlen(message)) / 2, message);
+  refresh();
 
-  do {
-    clear();
-    int maxX = getmaxx(window);
-    int maxY = getmaxy(window);
-    for (int y = 0; y < maxY; ++y)
-      for (int x = 0; x < maxX; ++x) {
-        if (x % (maxX / 10) == 0 || y % (maxY / 10) == 0)
-          mvaddch(y, x, '.');
-      }
-    refresh();
-  } while (getch() == KEY_RESIZE);
+  char name[64];
+  mvaddstr(rows - 2, columns / 2 - 5, "Your name?");
+  mvaddstr(rows - 1, columns / 2 - 5, "> ");
+
+  attron(COLOR_PAIR(NAME_INPUT_COLOR) | A_ITALIC);
+  getnstr(name, 63);
+  attroff(COLOR_PAIR(NAME_INPUT_COLOR) | A_ITALIC);
+
+  move(rows - 2, 0);
+  clrtobot();
+
+  char greeting[] = "Your name is ";
+  move(rows / 2 + 1, (columns - strlen(greeting) - strlen(name)) / 2);
+  printw("%s", greeting);
+  attron(COLOR_PAIR(GREET_COLOR) | A_BOLD);
+  printw("%s", name);
+  attroff(COLOR_PAIR(GREET_COLOR) | A_BOLD);
+  refresh();
+
+  getch();
   endwin();
+
   return 0;
 }
