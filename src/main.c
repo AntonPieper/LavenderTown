d2c0824325b06f7e179b5feb20527ac3ac0e5df1
Anton Pieper <94118572+AntonPieper@users.noreply.github.com>
2021-12-19 09:47:50 +0100

    feat: implement window (#23)
    Uses PDCurses on windows and Nurses on Unix-Like Systems (MacOS and Linux)

---
+++ src/main.c
@@ -0,0 +1,48 @@
+#include <curses.h>
+#include <string.h>
+
+const int GREET_COLOR = 1;
+const int NAME_INPUT_COLOR = 2;
+
+int main() {
+  WINDOW *window = initscr();
+  if (has_colors() == FALSE) {
+    endwin();
+    printf("Your Terminal does not support color!\n");
+    return 1;
+  }
+  start_color();
+  init_pair(GREET_COLOR, COLOR_YELLOW, COLOR_BLACK);
+  init_pair(NAME_INPUT_COLOR, COLOR_MAGENTA, COLOR_BLACK);
+  int rows, columns;
+  // A Macro for (rows = getmaxx(window), columns=getmaxy(window))
+  getmaxyx(window, rows, columns);
+  printw("Hello World %s!", "from me"); // Works just like printf(...)
+  char message[] = "Hello World from center of screen";
+  mvprintw(rows / 2, (columns - strlen(message)) / 2, message);
+  refresh();
+
+  char name[64];
+  mvaddstr(rows - 2, columns / 2 - 5, "Your name?");
+  mvaddstr(rows - 1, columns / 2 - 5, "> ");
+
+  attron(COLOR_PAIR(NAME_INPUT_COLOR));
+  getnstr(name, 63);
+  attroff(COLOR_PAIR(NAME_INPUT_COLOR));
+
+  move(rows - 2, 0);
+  clrtobot();
+
+  char greeting[] = "Your name is ";
+  move(rows / 2 + 1, (columns - strlen(greeting) - strlen(name)) / 2);
+  printw("%s", greeting);
+  attron(COLOR_PAIR(GREET_COLOR) | A_BOLD);
+  printw("%s", name);
+  attroff(COLOR_PAIR(GREET_COLOR) | A_BOLD);
+  refresh();
+
+  getch();
+  endwin();
+
+  return 0;
+}
