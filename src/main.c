3e1e2973c94c126c02e5240ce45cc4790a7d9f58
Azat Erol <azat.erol@informatik.hs-fulda.de>
2021-12-21 19:50:06 +0100

    feat: Implement Grid  #12

--- src/main.c
+++ src/main.c
@@ -1,48 +1,27 @@
-#include <curses.h>
-#include <string.h>
+#include <ncurses.h>
 
-const int GREET_COLOR = 1;
-const int NAME_INPUT_COLOR = 2;
+int main()
+{	
+	initscr();			/* Start curses mode 		  */
 
-int main() {
-  WINDOW *window = initscr();
-  if (has_colors() == FALSE) {
-    endwin();
-    printf("Your Terminal does not support color!\n");
-    return 1;
-  }
-  start_color();
-  init_pair(GREET_COLOR, COLOR_YELLOW, COLOR_BLACK);
-  init_pair(NAME_INPUT_COLOR, COLOR_MAGENTA, COLOR_BLACK);
-  int rows, columns;
-  // A Macro for (rows = getmaxx(window), columns=getmaxy(window))
-  getmaxyx(window, rows, columns);
-  printw("Hello World %s!", "from me"); // Works just like printf(...)
-  char message[] = "Hello World from center of screen";
-  mvprintw(rows / 2, (columns - strlen(message)) / 2, message);
-  refresh();
+  printw(
+			"____________________________________________\n"
+			"|  | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10|\n"
+		    "| A|   |   |   |   |   |   |   |   |   |   |\n"
+		    "| B|   |   |   |   |   |   |   |   |   |   |\n"
+			"| C|   |   |   |   |   |   |   |   |   |   |\n"
+			"| D|   |   |   |   |   |   |   |   |   |   |\n"
+			"| E|   |   |   |   |   |   |   |   |   |   |\n"
+			"| F|   |   |   |   |   |   |   |   |   |   |\n"
+			"| G|   |   |   |   |   |   |   |   |   |   |\n"
+			"| H|   |   |   |   |   |   |   |   |   |   |\n"
+			"| I|   |   |   |   |   |   |   |   |   |   |\n"
+			"| J|   |   |   |   |   |   |   |   |   |   |\n"
+			"--------------------------------------------\n");
 
-  char name[64];
-  mvaddstr(rows - 2, columns / 2 - 5, "Your name?");
-  mvaddstr(rows - 1, columns / 2 - 5, "> ");
+	refresh();			/* Print it on to the real screen */
+  getch();			/* Wait for user input */
+	endwin();			/* End curses mode		  */
 
-  attron(COLOR_PAIR(NAME_INPUT_COLOR));
-  getnstr(name, 63);
-  attroff(COLOR_PAIR(NAME_INPUT_COLOR));
-
-  move(rows - 2, 0);
-  clrtobot();
-
-  char greeting[] = "Your name is ";
-  move(rows / 2 + 1, (columns - strlen(greeting) - strlen(name)) / 2);
-  printw("%s", greeting);
-  attron(COLOR_PAIR(GREET_COLOR) | A_BOLD);
-  printw("%s", name);
-  attroff(COLOR_PAIR(GREET_COLOR) | A_BOLD);
-  refresh();
-
-  getch();
-  endwin();
-
-  return 0;
+	return 0;
 }
