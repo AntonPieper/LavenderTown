d89e60882cb1154476109b90f4c566e5c2e443c3
Azat Erol <azat.erol@informatik.hs-fulda.de>
2021-12-22 21:26:48 +0100

    Fixes #18: Implement User Interaction Cursor is shown on Terminal

--- src/main.c
+++ src/main.c
@@ -1,25 +1,39 @@
 #include <ncurses.h>
 
-int main() {
-  initscr(); /* Start curses mode 		  */
+struct setCoord {
+  int y, x;
+};
 
-  printw("____________________________________________\n"
-         "|  | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10|\n"
-         "| A|   |   |   |   |   |   |   |   |   |   |\n"
-         "| B|   |   |   |   |   |   |   |   |   |   |\n"
-         "| C|   |   |   |   |   |   |   |   |   |   |\n"
-         "| D|   |   |   |   |   |   |   |   |   |   |\n"
-         "| E|   |   |   |   |   |   |   |   |   |   |\n"
-         "| F|   |   |   |   |   |   |   |   |   |   |\n"
-         "| G|   |   |   |   |   |   |   |   |   |   |\n"
-         "| H|   |   |   |   |   |   |   |   |   |   |\n"
-         "| I|   |   |   |   |   |   |   |   |   |   |\n"
-         "| J|   |   |   |   |   |   |   |   |   |   |\n"
-         "--------------------------------------------\n");
+struct setCoord cursor;
+
+int main() {
+  initscr(); /* Start curses mode       */
+
+  int x, y;
+
+  cursor.y = 2;
+  cursor.x = 3;
+  clear();
+  noecho();
+
+  printw("________________________\n"
+         "| |1|2|3|4|5|6|7|8|9|10|\n"
+         "|A| | | | | | | | | | |\n"
+         "|B| | | | | | | | | | |\n"
+         "|C| | | | | | | | | | |\n"
+         "|D| | | | | | | | | | |\n"
+         "|E| | | | | | | | | | |\n"
+         "|F| | | | | | | | | | |\n"
+         "|G| | | | | | | | | | |\n"
+         "|H| | | | | | | | | | |\n"
+         "|I| | | | | | | | | | |\n"
+         "|J| | | | | | | | | | |\n"
+         "________________________\n");
 
   refresh(); /* Print it on to the real screen */
   getch();   /* Wait for user input */
-  endwin();  /* End curses mode		  */
+  endwin();  /* End curses mode     */
 
   return 0;
 }
+
