250ce2dc6e7736787b159336e6e2a726b769f891
Azat Erol <azat.erol@informatik.hs-fulda.de>
2021-12-23 14:26:43 +0100

    style: clean the code

--- src/main.c
+++ src/main.c
@@ -33,31 +33,34 @@ int main() {
 
   refresh(); /* Print it on to the real screen */
   getch();   /* Wait for user input */
-moveCursor(1);
-  endwin();  /* End curses mode     */
+  moveCursor(1);
+  endwin(); /* End curses mode     */
 
   return 0;
 }
 
-
 int moveCursor(int cMoves) {
-	while(cMoves) {
-		switch(getch()) {
-			case 'a':
-				if(cursor.x >3) cursor.x -= 2;
-				break;
-			case 'd':
-				if(cursor.x < 20) cursor.x += 2;
-				break;
-			case 'w':
-			if(cursor.y > 2) cursor.y -= 1;
-				break;
-			case 's':
-				if(cursor.y < 11) cursor.y += 1;
-				break;
-		}
-  	move(cursor.y,cursor.x);
-	  refresh();
-	}
+  while (cMoves) {
+    switch (getch()) {
+    case 'a':
+      if (cursor.x > 3)
+        cursor.x -= 2;
+      break;
+    case 'd':
+      if (cursor.x < 20)
+        cursor.x += 2;
+      break;
+    case 'w':
+      if (cursor.y > 2)
+        cursor.y -= 1;
+      break;
+    case 's':
+      if (cursor.y < 11)
+        cursor.y += 1;
+      break;
+    }
+    move(cursor.y, cursor.x);
+    refresh();
+  }
   return 1;
 }
