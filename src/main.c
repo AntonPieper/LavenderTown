b5dc40865d3f5400ad4b719168e0b5b2970d6a3b
Azat Erol <80638377+4zatero7@users.noreply.github.com>
2021-12-30 13:17:51 +0100

    feat: add intermediate user interaction (#20)
    edited by Anton Pieper <94118572+AntonPieper@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -4,15 +4,11 @@ struct setCoord {
   int y, x;
 };
 
-int main() {
-  initscr(); /* Start curses mode 		  */
-
 struct setCoord cursor;
 
 int main() {
-  initscr(); /* Start curses mode       */
 
-  int x, y;
+  initscr(); /* Start curses mode       */
 
   cursor.y = 2;
   cursor.x = 3;
@@ -21,23 +17,49 @@ int main() {
 
   printw("________________________\n"
          "| |1|2|3|4|5|6|7|8|9|10|\n"
-         "|A| | | | | | | | | | |\n"
-         "|B| | | | | | | | | | |\n"
-         "|C| | | | | | | | | | |\n"
-         "|D| | | | | | | | | | |\n"
-         "|E| | | | | | | | | | |\n"
-         "|F| | | | | | | | | | |\n"
-         "|G| | | | | | | | | | |\n"
-         "|H| | | | | | | | | | |\n"
-         "|I| | | | | | | | | | |\n"
-         "|J| | | | | | | | | | |\n"
+         "|A| | | | | | | | | |  |\n"
+         "|B| | | | | | | | | |  |\n"
+         "|C| | | | | | | | | |  |\n"
+         "|D| | | | | | | | | |  |\n"
+         "|E| | | | | | | | | |  |\n"
+         "|F| | | | | | | | | |  |\n"
+         "|G| | | | | | | | | |  |\n"
+         "|H| | | | | | | | | |  |\n"
+         "|I| | | | | | | | | |  |\n"
+         "|J| | | | | | | | | |  |\n"
          "________________________\n");
-
   move(cursor.y, cursor.x);
 
   refresh(); /* Print it on to the real screen */
   getch();   /* Wait for user input */
-  endwin();  /* End curses mode     */
+  moveCursor(1);
+  endwin(); /* End curses mode     */
 
   return 0;
+}
+
+int moveCursor(int cMoves) {
+  while (cMoves) {
+    switch (getch()) {
+    case 'a':
+      if (cursor.x > 3)
+        cursor.x -= 2;
+      break;
+    case 'd':
+      if (cursor.x < 20)
+        cursor.x += 2;
+      break;
+    case 'w':
+      if (cursor.y > 2)
+        cursor.y -= 1;
+      break;
+    case 's':
+      if (cursor.y < 11)
+        cursor.y += 1;
+      break;
+    }
+    move(cursor.y, cursor.x);
+    refresh();
+  }
+  return 1;
 }
